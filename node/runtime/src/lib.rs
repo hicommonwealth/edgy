4e2c9ab86db5c52fb7c248694c5ce372ef8941bd
Drew Stone <drewstone329@gmail.com>
2019-11-18 15:59:28 +0200

    Idk

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -20,12 +20,6 @@
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit="256"]
 
-use edge_identity::identity;
-// use edge_signaling::signaling;
-// use edge_treasury_reward::treasury_reward;
-// use edge_voting::voting;
-
-
 use rstd::prelude::*;
 use support::{
 	construct_runtime, parameter_types, traits::{SplitTwoWays, Currency, Randomness}
@@ -476,22 +470,22 @@ impl nicks::Trait for Runtime {
 }
 
 
-impl identity::Trait for Runtime {
+// impl edge_identity::Trait for Runtime {
+// 	type Event = Event;
+// 	type Currency = Balances;
+// }
+
+// impl edge_signaling::Trait for Runtime {
+// 	type Event = Event;
+// 	type Currency = Balances;
+// }
+
+impl edge_treasury_reward::Trait for Runtime {
 	type Event = Event;
 	type Currency = Balances;
 }
 
-// impl signaling::Trait for Runtime {
-// 	type Event = Event;
-// 	type Currency = Balances;
-// }
-
-// impl treasury_reward::Trait for Runtime {
-// 	type Event = Event;
-// 	type Currency = Balances;
-// }
-
-// impl voting::Trait for Runtime {
+// impl edge_voting::Trait for Runtime {
 // 	type Event = Event;
 // }
 
@@ -556,10 +550,10 @@ construct_runtime!(
 		Offences: offences::{Module, Call, Storage, Event},
 		RandomnessCollectiveFlip: randomness_collective_flip::{Module, Call, Storage},
 		Nicks: nicks::{Module, Call, Storage, Event<T>},
-		Identity: identity::{Module, Call, Storage, Config<T>, Event<T>},
-		// Signaling: signaling::{Module, Call, Storage, Config<T>, Event<T>},
-		// Voting: voting::{Module, Call, Storage, Event<T>},
-		// TreasuryReward: treasury_reward::{Module, Call, Storage, Config<T>, Event<T>},
+		// Identity: edge_identity::{Module, Call, Storage, Config<T>, Event<T>},
+		// Signaling: edge_signaling::{Module, Call, Storage, Config<T>, Event<T>},
+		// Voting: edge_voting::{Module, Call, Storage, Event<T>},
+		TreasuryReward: edge_treasury_reward::{Module, Call, Storage, Config<T>, Event<T>},
 	}
 );
 
